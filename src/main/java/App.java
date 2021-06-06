31692cf5b8df007012d6fe103d99d1be24b149a1
theluiszone <lacostamejia@knights.ucf.edu>
2021-06-06 14:34:36 -0400

    Not completed

---
+++ src/main/java/App.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Luis Andres Acosta Mejia
+ *
+ *
+ * What is your age? 15
+You are not old enough to legally drive.
+*
+* What is your age? 35
+You are old enough to legally drive.
+ */
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in); //Scanner method declared
+
+        //These are variables that are going to be using
+        int age;
+        int q;
+
+        System.out.print("What is your age? ");
+        age = in.nextInt();
+
+        if(age >= 16)
+        {
+            q = 1;
+        }
+
+
+
+    }
+}
